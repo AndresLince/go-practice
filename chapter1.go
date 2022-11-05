bf39a54859a828e7727e1e48aa8017fa81707998
AndresLince <lynxdarmatron@gmail.com>
2022-11-05 13:17:13 -0500

    Cracking the coding interview exercise 2 chapter 1 Array and Strings

--- chapter1.go
+++ chapter1.go
@@ -1,7 +1,8 @@
 package main
 
 /*
-	Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+	Is Unique: Implement an algorithm to determine if a string has all unique
+	characters. What if you
 	cannot use additional data structures?
 */
 
@@ -12,6 +13,36 @@ func HasAllUniqueCharacters(word string) bool {
 			return false
 		}
 		s[char] = 1
+	}
+	return true
+}
+
+/*
+	Check Permutation: Given two strings, write a method to decide if one is a
+	permutation of the other.
+*/
+
+func CheckPermutation(word1, word2 string) bool {
+	string1Length := len([]rune(word1))
+	string2Length := len([]rune(word2))
+	if string1Length != string2Length {
+		return false
+	}
+	var letters = make(map[rune]int)
+	for _, char := range word1 {
+		if letters[char] != 0 {
+			return false
+		}
+		letters[char] = 1
+	}
+	for _, char2 := range word2 {
+		if letters[char2] == 0 {
+			return false
+		}
+		letters[char2]--
+		if letters[char2] < 0 {
+			return false
+		}
 	}
 	return true
 }
