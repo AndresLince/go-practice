90b84b7d2db00dda546051b50a9821eea8cbad7b
AndresLince <lynxdarmatron@gmail.com>
2022-11-05 13:17:32 -0500

    Cracking the coding interview exercise 2 chapter 1 tests Array and Strings

--- chapter1_test.go
+++ chapter1_test.go
@@ -13,6 +13,16 @@ func TestHasAllUniqueCharacters(t *testing.T) {
 	}
 	response = HasAllUniqueCharacters("algoa")
 	if response != false {
-		t.Fatalf(`HasAllUniqueCharacters("algo") expect false`)
+		t.Fatalf(`HasAllUniqueCharacters("algoa") expect false`)
+	}
+}
+func TestCheckPermutation(t *testing.T) {
+	response := CheckPermutation("algo", "loga")
+	if response != true {
+		t.Fatalf(`CheckPermutation("algo", "loga") expect true`)
+	}
+	response = CheckPermutation("algoa", "algo")
+	if response != false {
+		t.Fatalf(`CheckPermutation("algoa", "algo") expect false`)
 	}
 }
