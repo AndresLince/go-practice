ad859bf9e8ff44639240c36150f03cd66900871c
AndresLince <lynxdarmatron@gmail.com>
2022-11-06 11:41:14 -0500

    Cracking the coding interview exercise 3 chapter 1 Array and Strings

--- chapter1.go
+++ chapter1.go
@@ -46,3 +46,47 @@ func CheckPermutation(word1, word2 string) bool {
 	}
 	return true
 }
+
+/*
+	URLify: Write a method to replace all spaces in a string with '%20'. You
+	may assume that the string has sufficient space at the end to hold the
+	additional characters,and that you are given the "true" length of the
+	string. (Note: If implementing in Java, please use a character array so
+	that you can perform this operation in place.)
+*/
+func Urlify(word string, length int) string {
+	emptyChars := countChars(word, 32, 0, length)
+	longIndex := length + (emptyChars * 2) - 1
+	shortIndex := length - 1
+	for shortIndex >= 0 {
+		if word[shortIndex] == 32 {
+			word = setCharAt(word, longIndex, 37)
+			word = setCharAt(word, longIndex-1, 48)
+			word = setCharAt(word, longIndex-2, 50)
+			longIndex = longIndex - 3
+		} else {
+			word = setCharAt(word, longIndex, rune(word[shortIndex]))
+			longIndex--
+		}
+		shortIndex--
+	}
+	return word
+}
+
+func countChars(word string, char byte, from int, to int) int {
+	counter := 0
+	for from < to {
+		if char == word[from] {
+			counter++
+		}
+		from++
+	}
+	return counter
+}
+
+func setCharAt(word string, index int, char rune) string {
+	if index > len(word) {
+		return word
+	}
+	return word[:index] + string(char) + word[index+1:]
+}
