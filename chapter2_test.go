c0e1d8ce837938b195f0522bcd11f307bb0eb3c0
AndresLince <lynxdarmatron@gmail.com>
2022-11-19 09:54:56 -0500

    Cracking the coding interview exercise 2 chapter 2 tests Linked Lists

---
+++ chapter2_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestKthToLast(t *testing.T) {
+	linkedList := Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	expectedLinkedList := &Node{5, &Node{6, &Node{7, nil}}}
+	response := KthToLast(linkedList, 2)
+	fmt.Println(response)
+	fmt.Println(expectedLinkedList)
+	if !reflect.DeepEqual(response, expectedLinkedList) {
+		t.Fatalf(`KthToLast(linkedList, 2) expect a valid linked list`)
+	}
+}
