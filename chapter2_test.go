422ca451f2dd9b6c37f72814972c5546e228f38c
AndresLince <lynxdarmatron@gmail.com>
2022-11-20 13:54:06 -0500

    Cracking the coding interview exercise 3 chapter 2 tests Linked Lists

--- chapter2_test.go
+++ chapter2_test.go
@@ -16,3 +16,12 @@ func TestKthToLast(t *testing.T) {
 		t.Fatalf(`KthToLast(linkedList, 2) expect a valid linked list`)
 	}
 }
+
+func TestDeleteMiddleNode(t *testing.T) {
+	linkedList := Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	expectedLinkedList := &Node{1, &Node{2, &Node{3, &Node{5, &Node{6, &Node{7, nil}}}}}}
+	result := DeleteMiddleNode(linkedList)
+	if !reflect.DeepEqual(result, expectedLinkedList) {
+		t.Fatalf(`DeleteMiddleNode() expect valid linkedList`)
+	}
+}
