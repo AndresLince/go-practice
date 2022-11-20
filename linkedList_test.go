fd1f109aee3cc317cd9ec3ee7089842537599a07
AndresLince <lynxdarmatron@gmail.com>
2022-11-20 13:49:09 -0500

    Add linkedList Delete node function tests

--- linkedList_test.go
+++ linkedList_test.go
@@ -14,3 +14,29 @@ func TestAppendToTail(t *testing.T) {
 		t.Fatalf(`AppendToTail() expect valid linkedList`)
 	}
 }
+
+func TestDeleteNode(t *testing.T) {
+	linkedList := Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	expectedLinkedList := &Node{1, &Node{2, &Node{3, &Node{5, &Node{6, &Node{7, nil}}}}}}
+	result := linkedList.DeleteNode(4)
+	if !reflect.DeepEqual(result, expectedLinkedList) {
+		t.Fatalf(`DeleteNode() expect valid linkedList`)
+	}
+	linkedList = Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	expectedLinkedList = &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}
+	result = linkedList.DeleteNode(1)
+	if !reflect.DeepEqual(result, expectedLinkedList) {
+		t.Fatalf(`DeleteNode() expect valid linkedList`)
+	}
+	linkedList = Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	expectedLinkedList = &Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, nil}}}}}}
+	result = linkedList.DeleteNode(7)
+	if !reflect.DeepEqual(result, expectedLinkedList) {
+		t.Fatalf(`DeleteNode() expect valid linkedList`)
+	}
+	linkedList = Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
+	result = linkedList.DeleteNode(7)
+	if !reflect.DeepEqual(result, &linkedList) {
+		t.Fatalf(`DeleteNode() expect valid linkedList`)
+	}
+}
