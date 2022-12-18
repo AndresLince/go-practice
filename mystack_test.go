db1b11a01ac60c8e2acb6af8763d550173a05072
AndresLince <lynxdarmatron@gmail.com>
2022-12-17 21:10:55 -0500

    Cracking the coding interview chapter 3 Stack tests Stacks and Queues

---
+++ mystack_test.go
@@ -0,0 +1,70 @@
+package main
+
+import "testing"
+
+func TestIsEmpty(t *testing.T) {
+	stack := MyStack{}
+	if !stack.IsEmpty() {
+		t.Fatalf(`IsEmpty() expect true`)
+	}
+	stack.Push(1)
+	if stack.IsEmpty() {
+		t.Fatalf(`IsEmpty() expect false`)
+	}
+}
+
+func TestPeek(t *testing.T) {
+	stack := MyStack{}
+	if stack.Peek() != nil {
+		t.Fatalf(`TestPeek() expect nil`)
+	}
+	stack.Push(1)
+	if stack.Peek() != 1 {
+		t.Fatalf(`TestPeek() expect 1`)
+	}
+	stack.Push(2)
+	if stack.Peek() != 2 {
+		t.Fatalf(`TestPeek() expect 2`)
+	}
+	stack.Push(3)
+	if stack.Peek() != 3 {
+		t.Fatalf(`TestPeek() expect 3`)
+	}
+}
+
+func TestPop(t *testing.T) {
+	stack := MyStack{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	if stack.Peek() != 3 {
+		t.Fatalf(`TestPeek() expect 3`)
+	}
+	stack.Pop()
+	if stack.Peek() != 2 {
+		t.Fatalf(`TestPeek() expect 2`)
+	}
+	stack.Pop()
+	if stack.Peek() != 1 {
+		t.Fatalf(`TestPeek() expect 2`)
+	}
+	stack.Pop()
+	if stack.Peek() != nil {
+		t.Fatalf(`TestPeek() expect nil`)
+	}
+}
+func TestPush(t *testing.T) {
+	stack := MyStack{}
+	stack.Push(1)
+	if stack.Peek() != 1 {
+		t.Fatalf(`TestPeek() expect 1`)
+	}
+	stack.Push(2)
+	if stack.Peek() != 2 {
+		t.Fatalf(`TestPeek() expect 2`)
+	}
+	stack.Push(3)
+	if stack.Peek() != 3 {
+		t.Fatalf(`TestPeek() expect 3`)
+	}
+}
