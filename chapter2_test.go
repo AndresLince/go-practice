2c418dcb96d50eff6479df0bb3e43f660e0bfe2b
AndresLince <lynxdarmatron@gmail.com>
2023-01-09 19:36:51 -0500

    Cracking the coding interview chapter 2 tests refactor Array and Strings

--- chapter2_test.go
+++ chapter2_test.go
@@ -3,77 +3,79 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/AndresLince/go-practice/linkedList"
 )
 
 func TestKthToLast(t *testing.T) {
-	linkedList := Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
-	expectedLinkedList := &Node{5, &Node{6, &Node{7, nil}}}
-	response := KthToLast(linkedList, 2)
+	newLinkedList := linkedList.Node{1, &linkedList.Node{2, &linkedList.Node{3, &linkedList.Node{4, &linkedList.Node{5, &linkedList.Node{6, &linkedList.Node{7, nil}}}}}}}
+	expectedLinkedList := &linkedList.Node{5, &linkedList.Node{6, &linkedList.Node{7, nil}}}
+	response := KthToLast(newLinkedList, 2)
 	if !reflect.DeepEqual(response, expectedLinkedList) {
 		t.Fatalf(`KthToLast(linkedList, 2) expect a valid linked list`)
 	}
 }
 
 func TestDeleteMiddleNode(t *testing.T) {
-	linkedList := Node{1, &Node{2, &Node{3, &Node{4, &Node{5, &Node{6, &Node{7, nil}}}}}}}
-	expectedLinkedList := &Node{1, &Node{2, &Node{3, &Node{5, &Node{6, &Node{7, nil}}}}}}
-	result := DeleteMiddleNode(linkedList)
+	newLinkedList := linkedList.Node{1, &linkedList.Node{2, &linkedList.Node{3, &linkedList.Node{4, &linkedList.Node{5, &linkedList.Node{6, &linkedList.Node{7, nil}}}}}}}
+	expectedLinkedList := &linkedList.Node{1, &linkedList.Node{2, &linkedList.Node{3, &linkedList.Node{5, &linkedList.Node{6, &linkedList.Node{7, nil}}}}}}
+	result := DeleteMiddleNode(newLinkedList)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`DeleteMiddleNode() expect valid linkedList`)
 	}
 }
 
 func TestPartition(t *testing.T) {
-	linkedList := Node{3, &Node{5, &Node{8, &Node{5, &Node{10, &Node{2, &Node{1, nil}}}}}}}
-	expectedLinkedList := Node{1, &Node{2, &Node{3, &Node{5, &Node{8, &Node{5, &Node{10, nil}}}}}}}
-	result := Partition(linkedList, 5)
+	newLinkedList := linkedList.Node{3, &linkedList.Node{5, &linkedList.Node{8, &linkedList.Node{5, &linkedList.Node{10, &linkedList.Node{2, &linkedList.Node{1, nil}}}}}}}
+	expectedLinkedList := linkedList.Node{1, &linkedList.Node{2, &linkedList.Node{3, &linkedList.Node{5, &linkedList.Node{8, &linkedList.Node{5, &linkedList.Node{10, nil}}}}}}}
+	result := Partition(newLinkedList, 5)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`Partition() expect valid linkedList`)
 	}
 }
 
 func TestSumLists(t *testing.T) {
-	linkedList1 := Node{4, &Node{3, &Node{4, nil}}}
-	linkedList2 := Node{1, &Node{4, nil}}
-	expectedLinkedList := Node{5, &Node{7, &Node{4, nil}}}
-	linkedListAdder := NewLinkedListAdder(LinkedListPrependAdder{})
+	linkedList1 := linkedList.Node{4, &linkedList.Node{3, &linkedList.Node{4, nil}}}
+	linkedList2 := linkedList.Node{1, &linkedList.Node{4, nil}}
+	expectedLinkedList := linkedList.Node{5, &linkedList.Node{7, &linkedList.Node{4, nil}}}
+	linkedListAdder := linkedList.NewLinkedListAdder(linkedList.LinkedListPrependAdder{})
 	result := SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumLists() expect valid linkedList`)
 	}
-	linkedList1 = Node{2, &Node{3, nil}}
-	linkedList2 = Node{1, &Node{4, &Node{8, nil}}}
-	expectedLinkedList = Node{3, &Node{7, &Node{8, nil}}}
+	linkedList1 = linkedList.Node{2, &linkedList.Node{3, nil}}
+	linkedList2 = linkedList.Node{1, &linkedList.Node{4, &linkedList.Node{8, nil}}}
+	expectedLinkedList = linkedList.Node{3, &linkedList.Node{7, &linkedList.Node{8, nil}}}
 	result = SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumLists() expect valid linkedList`)
 	}
-	linkedList1 = Node{7, &Node{1, &Node{6, nil}}}
-	linkedList2 = Node{5, &Node{9, &Node{2, nil}}}
-	expectedLinkedList = Node{2, &Node{1, &Node{9, nil}}}
+	linkedList1 = linkedList.Node{7, &linkedList.Node{1, &linkedList.Node{6, nil}}}
+	linkedList2 = linkedList.Node{5, &linkedList.Node{9, &linkedList.Node{2, nil}}}
+	expectedLinkedList = linkedList.Node{2, &linkedList.Node{1, &linkedList.Node{9, nil}}}
 	result = SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumLists() expect valid linkedList`)
 	}
-	linkedList1 = Node{9, &Node{9, &Node{9, nil}}}
-	linkedList2 = Node{9, &Node{9, &Node{9, nil}}}
-	expectedLinkedList = Node{8, &Node{9, &Node{9, &Node{1, nil}}}}
+	linkedList1 = linkedList.Node{9, &linkedList.Node{9, &linkedList.Node{9, nil}}}
+	linkedList2 = linkedList.Node{9, &linkedList.Node{9, &linkedList.Node{9, nil}}}
+	expectedLinkedList = linkedList.Node{8, &linkedList.Node{9, &linkedList.Node{9, &linkedList.Node{1, nil}}}}
 	result = SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumLists() expect valid linkedList`)
 	}
 
-	linkedListAdder = NewLinkedListAdder(LinkedListAppendToTailAdder{})
-	linkedList1 = Node{4, &Node{3, &Node{1, nil}}}
-	linkedList2 = Node{4, &Node{1, nil}}
-	expectedLinkedList = Node{1, &Node{4, &Node{8, nil}}}
+	linkedListAdder = linkedList.NewLinkedListAdder(linkedList.LinkedListAppendToTailAdder{})
+	linkedList1 = linkedList.Node{4, &linkedList.Node{3, &linkedList.Node{1, nil}}}
+	linkedList2 = linkedList.Node{4, &linkedList.Node{1, nil}}
+	expectedLinkedList = linkedList.Node{1, &linkedList.Node{4, &linkedList.Node{8, nil}}}
 	result = SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`SumLists() expect valid linkedList`)
 	}
-	linkedList1 = Node{7, &Node{1, &Node{6, nil}}}
-	linkedList2 = Node{5, &Node{9, &Node{2, nil}}}
-	expectedLinkedList = Node{9, &Node{1, &Node{2, nil}}}
+	linkedList1 = linkedList.Node{7, &linkedList.Node{1, &linkedList.Node{6, nil}}}
+	linkedList2 = linkedList.Node{5, &linkedList.Node{9, &linkedList.Node{2, nil}}}
+	expectedLinkedList = linkedList.Node{9, &linkedList.Node{1, &linkedList.Node{2, nil}}}
 	result = SumLists(linkedList1, linkedList2, *linkedListAdder)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`SumLists() expect valid linkedList`)
@@ -81,16 +83,16 @@ func TestSumLists(t *testing.T) {
 }
 
 func TestSumListsForwardOrder(t *testing.T) {
-	linkedList1 := Node{1, &Node{3, &Node{4, nil}}}
-	linkedList2 := Node{1, &Node{4, nil}}
-	expectedLinkedList := Node{1, &Node{4, &Node{8, nil}}}
+	linkedList1 := linkedList.Node{1, &linkedList.Node{3, &linkedList.Node{4, nil}}}
+	linkedList2 := linkedList.Node{1, &linkedList.Node{4, nil}}
+	expectedLinkedList := linkedList.Node{1, &linkedList.Node{4, &linkedList.Node{8, nil}}}
 	result := SumListsForwardOrder(linkedList1, linkedList2)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumListsForwardOrder() expect valid linkedList`)
 	}
-	linkedList1 = Node{6, &Node{1, &Node{7, nil}}}
-	linkedList2 = Node{2, &Node{9, &Node{5, nil}}}
-	expectedLinkedList = Node{9, &Node{1, &Node{2, nil}}}
+	linkedList1 = linkedList.Node{6, &linkedList.Node{1, &linkedList.Node{7, nil}}}
+	linkedList2 = linkedList.Node{2, &linkedList.Node{9, &linkedList.Node{5, nil}}}
+	expectedLinkedList = linkedList.Node{9, &linkedList.Node{1, &linkedList.Node{2, nil}}}
 	result = SumListsForwardOrder(linkedList1, linkedList2)
 	if !reflect.DeepEqual(result, expectedLinkedList) {
 		t.Fatalf(`TestSumListsForwardOrder() expect valid linkedList`)
@@ -98,32 +100,32 @@ func TestSumListsForwardOrder(t *testing.T) {
 }
 
 func TestIsPalindrome(t *testing.T) {
-	linkedList := &Node{"D", &Node{"a", &Node{"b", &Node{"a", &Node{"l",
-		&Node{"e", &Node{"a", &Node{"r", &Node{"r", &Node{"o", &Node{"z",
-			&Node{"a", &Node{"l", &Node{"a", &Node{"z", &Node{"o",
-				&Node{"r", &Node{"r", &Node{"a", &Node{"e", &Node{"l",
-					&Node{"a", &Node{"b", &Node{"a", &Node{"d", nil}}}}}}}}}}}}}}}}}}}}}}}}}
-	result := IsPalindrome(linkedList)
+	newlinkedList := &linkedList.Node{"D", &linkedList.Node{"a", &linkedList.Node{"b", &linkedList.Node{"a", &linkedList.Node{"l",
+		&linkedList.Node{"e", &linkedList.Node{"a", &linkedList.Node{"r", &linkedList.Node{"r", &linkedList.Node{"o", &linkedList.Node{"z",
+			&linkedList.Node{"a", &linkedList.Node{"l", &linkedList.Node{"a", &linkedList.Node{"z", &linkedList.Node{"o",
+				&linkedList.Node{"r", &linkedList.Node{"r", &linkedList.Node{"a", &linkedList.Node{"e", &linkedList.Node{"l",
+					&linkedList.Node{"a", &linkedList.Node{"b", &linkedList.Node{"a", &linkedList.Node{"d", nil}}}}}}}}}}}}}}}}}}}}}}}}}
+	result := IsPalindrome(newlinkedList)
 	if result != true {
 		t.Fatalf(`TestIsPalindrome() expect true`)
 	}
-	linkedList = &Node{"P", &Node{"e", &Node{"r", &Node{"r", &Node{"o", nil}}}}}
-	result = IsPalindrome(linkedList)
+	newlinkedList = &linkedList.Node{"P", &linkedList.Node{"e", &linkedList.Node{"r", &linkedList.Node{"r", &linkedList.Node{"o", nil}}}}}
+	result = IsPalindrome(newlinkedList)
 	if result != false {
 		t.Fatalf(`TestIsPalindrome() expect false`)
 	}
 }
 
 func TestIsIntersection(t *testing.T) {
-	intersectionNode := &Node{"d", &Node{"a", &Node{"s", nil}}}
-	linkedList1 := &Node{"d", &Node{"a", &Node{"z", intersectionNode}}}
-	linkedList2 := &Node{"p", &Node{"e", &Node{"r", &Node{"r", &Node{"o", intersectionNode}}}}}
+	intersectionNode := &linkedList.Node{"d", &linkedList.Node{"a", &linkedList.Node{"s", nil}}}
+	linkedList1 := &linkedList.Node{"d", &linkedList.Node{"a", &linkedList.Node{"z", intersectionNode}}}
+	linkedList2 := &linkedList.Node{"p", &linkedList.Node{"e", &linkedList.Node{"r", &linkedList.Node{"r", &linkedList.Node{"o", intersectionNode}}}}}
 	result := IsIntersection(linkedList1, linkedList2)
 	if !reflect.DeepEqual(result, intersectionNode) {
 		t.Fatalf(`TestIsIntersection() expect a valid linked list`)
 	}
-	linkedList1 = &Node{"a", &Node{"b", &Node{"c", nil}}}
-	linkedList2 = &Node{"z", &Node{"b", &Node{"c", nil}}}
+	linkedList1 = &linkedList.Node{"a", &linkedList.Node{"b", &linkedList.Node{"c", nil}}}
+	linkedList2 = &linkedList.Node{"z", &linkedList.Node{"b", &linkedList.Node{"c", nil}}}
 	result = IsIntersection(linkedList1, linkedList2)
 	if result != nil {
 		t.Fatalf(`TestIsIntersection() expect nil`)
@@ -131,16 +133,16 @@ func TestIsIntersection(t *testing.T) {
 }
 
 func TestLoopDetection(t *testing.T) {
-	linkedList := &Node{"d", &Node{"a", &Node{"z", nil}}}
-	linkedList.next.next.next = linkedList
-	result := LoopDetection(linkedList)
-	if !reflect.DeepEqual(result.next, linkedList) {
+	newLinkedList := &linkedList.Node{"d", &linkedList.Node{"a", &linkedList.Node{"z", nil}}}
+	newLinkedList.Next.Next.Next = newLinkedList
+	result := LoopDetection(newLinkedList)
+	if !reflect.DeepEqual(result.Next, newLinkedList) {
 		t.Fatalf(`TestLoopDetection() expect a valid linked list`)
 	}
-	tail := &Node{"d", &Node{"a", &Node{"z", nil}}}
-	linkedList = &Node{"d", &Node{"a", &Node{"z", nil}}}
-	linkedList.next.next.next = tail
-	result = LoopDetection(linkedList)
+	tail := &linkedList.Node{"d", &linkedList.Node{"a", &linkedList.Node{"z", nil}}}
+	newLinkedList = &linkedList.Node{"d", &linkedList.Node{"a", &linkedList.Node{"z", nil}}}
+	newLinkedList.Next.Next.Next = tail
+	result = LoopDetection(newLinkedList)
 	if result != nil {
 		t.Fatalf(`TestIsIntersection() expect nil`)
 	}
