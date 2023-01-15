64f552edc4d2a40a4a3ce789a33ad7c862e2976e
AndresLince <lynxdarmatron@gmail.com>
2023-01-15 00:42:38 -0500

    Cracking the coding interview chapter 3 exercise 4 Stacks and Queues

--- queue/queueWithStack.go
+++ queue/queueWithStack.go
@@ -12,6 +12,7 @@ import (
 type QueueWithStack struct {
 	stack1 stacks.MyStack
 	stack2 stacks.MyStack
+	head   interface{}
 }
 
 func NewQueueWithStack() *QueueWithStack {
@@ -22,16 +23,32 @@ func NewQueueWithStack() *QueueWithStack {
 }
 
 func (queueWithStack *QueueWithStack) Add(data interface{}) {
+	if queueWithStack.stack1.IsEmpty() {
+		queueWithStack.head = data
+	}
 	queueWithStack.stack1.Push(data)
 }
 
-func (queueWithStack QueueWithStack) Remove() interface{} {
+func (queueWithStack *QueueWithStack) Remove() interface{} {
 	for !queueWithStack.stack1.IsEmpty() {
 		queueWithStack.stack2.Push(queueWithStack.stack1.Pop())
 	}
 	data := queueWithStack.stack2.Pop()
+	if queueWithStack.stack2.IsEmpty() {
+		queueWithStack.head = nil
+	} else {
+		queueWithStack.head = queueWithStack.stack2.Peek()
+	}
 	for !queueWithStack.stack2.IsEmpty() {
 		queueWithStack.stack1.Push(queueWithStack.stack2.Pop())
 	}
 	return data
+}
+
+func (queueWithStack QueueWithStack) Peak() interface{} {
+	return queueWithStack.head
+}
+
+func (queueWithStack QueueWithStack) IsEmpty() bool {
+	return queueWithStack.stack1.IsEmpty()
 }
